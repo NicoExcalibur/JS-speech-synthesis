81a8ef783cf8100f2bcdb20ac3eddd4c2b15e9df
NicoExcalibur <nico.zanz@gmail.com>
2021-07-01 14:39:06 +0200

    change options

--- app.js
+++ app.js
@@ -21,5 +21,21 @@ function setVoice() {
   msg.voice = voices.find((voice) => voice.name === this.value);
 }
 
+// stop the voice at the change
+function toggle(startOver = true) {
+  speechSynthesis.cancel();
+  if (startOver) {
+    speechSynthesis.speak(msg);
+  }
+}
+
+function setOption() {
+  console.log(this.name, this.value);
+  msg[this.name] = this.value;
+}
+
 speechSynthesis.addEventListener("voiceschanged", populateVoices);
 voicesDropdown.addEventListener("change", setVoice);
+options.forEach((option) => option.addEventListener("change", setOption));
+speakButton.addEventListener("click", toggle);
+stopButton.addEventListener("click", () => toggle(false));
